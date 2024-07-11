acae6760078bed11a48e4a80dcf9f9d385c3f255
Mark Glinberg <t-mglinberg@microsoft.com>
2024-07-11 16:07:19 -0400

    Working draft with hashes and fqdn-yang script

--- fqdn.py
+++ fqdn.py
@@ -1,15 +1,19 @@
 import json
 import subprocess
 import hashlib
+import ipaddress
 
-def createPatch(path, op, template=None, ip=None):
+PREFIX = "FQDN_RULE_"
+
+def createPatch(path, op, template=None, ip=None, src_dst=None):
     """Creates a patch entry for the Generic Config Updater (GCU)
 
     Args:
         path ("String"): path of the rule to patch (<TABLE_NAME>|<RULE_NAME>)
         op ("String"): "add" or "remove"
-        template ("String", optional): template of the rule if op="add". Defaults to None
+        template ("Dict", optional): template of the rule if op="add". Defaults to None
         ip ("String", optional): ip of the rule if op="add". Defaults to None.
+        ip ("String", optional): whether the domain is the "SRC" or "DST" for the rule traffic if op="add". Defaults to None.
 
     Returns:
         dict: the patch for that rule to be read by the GCU
@@ -18,17 +22,19 @@ def createPatch(path, op, template=None, ip=None):
     patch["op"] = op
     patch["path"] = "/ACL_RULE/" + path
     if op == "add":
-        rule = template.replace("((FQDN_IP))", ip)
+        rule = dict(template)
 
-        rule = rule.replace("\"", "")
-        rule = rule.replace("\n", " ")
+        address, mask = ip.split("/")
+        ipaddress_obj = ipaddress.ip_address(address)
+        
+        if ipaddress_obj.version == 4:
+            ip_type = "IP"
+        else:
+            ip_type = "IPV6"
 
-        lines = rule.split(", ")
-        value = {}
-        for line in lines:
-            entry = line.split(": ")
-            value[entry[0]] = entry[1]
-        patch["value"] = value
+        rule[src_dst + "_" + ip_type] = ip
+
+        patch["value"] = rule
     return patch
 
 def getExistingRules():
@@ -39,50 +45,52 @@ def getExistingRules():
     Returns:
         dict: nested mapping of rules with keys: table -> domain -> hash -> list of rules with matching templates but different ips
     """
-    PREFIX = "FQDN_RULE_"
     rules = {}
 
-    sonic_out = subprocess.run("sonic-cfggen -d --var-json ACL_RULE", shell=True, capture_output=True, text=True)
-    existing_rules = json.loads(sonic_out.stdout)
-    # with open("rules.json") as f: # Need to replace with the above commands
-    #     existing_rules = json.load(f)
+    # sonic_out = subprocess.run("sonic-cfggen -d --var-json ACL_RULE", shell=True, capture_output=True, text=True)
+    # existing_rules = json.loads(sonic_out.stdout)
+    with open("rules.json") as f: # Need to replace with the above commands
+        existing_rules = json.load(f)
 
-    for k, v in existing_rules.items():
-        table, name = k.split("|")
+    for key, rule in existing_rules.items():
+        table, name = key.split("|")
         if PREFIX == name[:len(PREFIX)]:
-            pre, fix, domain, hash, number = name.split("_")
+            pre, fix, src_dst, domain, hash, number = name.split("_")
 
             value = {}
             value["number"] = int(number)
+            value["src_dst"] = src_dst
 
-            ip_source_types = ["DST_IP",
-                              "DST_IPV6",
-                              "SRC_IP",
-                              "SRC_IPV6"]
+            ip_types = ["IP", "IPV6"]
             
-            for type in ip_source_types:
-                if type in v:
-                    value["ip"] = v[type]
-                    v[type] = "((FQDN_IP))"
+            for ip_type in ip_types:
+                key = src_dst + "_" + ip_type
+                if key in rule:
+                    value["ip"] = rule[key]
+                    rule.pop(key)
                     break
 
-            value["rule"] = str(v)
-            value["rule"] = value["rule"].replace("{", "")
-            value["rule"] = value["rule"].replace("}", "")
-            value["rule"] = value["rule"].replace("'", "")
+            value["rule"] = rule
             
             if table not in rules:
                 rules[table] = {}
 
-            if domain not in rules[table]:
-                rules[table][domain] = {}
+            if src_dst not in rules[table]:
+                rules[table][src_dst] = {}
 
-            if hash not in rules[table][domain]:
-                rules[table][domain][hash] = []
+            if domain not in rules[table][src_dst]:
+                rules[table][src_dst][domain] = {}
 
-            rules[table][domain][hash].append(value)
+            if hash not in rules[table][src_dst][domain]:
+                rules[table][src_dst][domain][hash] = []
+
+            rules[table][src_dst][domain][hash].append(value)
     
     return rules
+# rules = getExistingRules()
+
+# with open("out.json", "w") as f:
+#     json.dump(rules, f, indent=4)
 
 def getTemplates():
     """Queries SONiC for all existing templates and returns them as a list
@@ -90,11 +98,26 @@ def getTemplates():
     Returns:
         list: dictionaries of all existing templates
     """
-    sonic_out = subprocess.run("sonic-cfggen -d --var-json FQDN_ACL_RULE_TEMPLATE", shell=True, capture_output=True, text=True)
-    templates = json.loads(sonic_out.stdout)
-    # with open("templates.json") as f: # need to replace with the above commands
-    #     templates = json.load(f)
+    # sonic_out = subprocess.run("sonic-cfggen -d --var-json FQDN_ACL_RULE_TEMPLATE", shell=True, capture_output=True, text=True)
+    # templates = json.loads(sonic_out.stdout)
+    with open("templates.json") as f: # need to replace with the above commands
+        templates = json.load(f)
+    for _, template in templates.items():
+        template["RULE_TEMPLATE"] = {}
+        template_keys = [k for k, v in template.items()]
+        for k in template_keys:
+            if not (k == "ACL_TABLE_NAME" or k == "SRC_DOMAIN" or k == "DST_DOMAIN" or k == "RULE_TEMPLATE"):
+                template["RULE_TEMPLATE"][k] = template[k]
+                del template[k]
     return templates
+# templates = getTemplates()
+
+# with open("out.json", "w") as f:
+#     json.dump(templates, f, indent=4)
+
+# for _, template in templates.items():
+#     hash = hashlib.sha256((json.dumps(template["RULE_TEMPLATE"], sort_keys=True)).encode(encoding="utf-8", errors="replace")).hexdigest()[:5]
+#     print(hash)
 
 def fake_nslookup(domain):
     """Temporary function that returns fake IP addresses for a given domain in nslookup.json
@@ -118,7 +141,6 @@ def updateRules():
     Returns:
         list: dictionaries for each patch
     """
-    PREFIX = "FQDN_RULE_"
     patches = []
     templates = getTemplates()
     existing_rules = getExistingRules()
@@ -126,23 +148,31 @@ def updateRules():
 
     # only replaces ips, doesn't update rules if they were changed
     for _, template in templates.items():
-        domain = template["DOMAIN"][:7]
-        hash = hashlib.sha256(template["RULE_TEMPLATE"].encode(encoding="utf-8", errors="replace")).hexdigest()[:5]
-        rule_name = PREFIX + domain + "_" + hash + "_"
+        if "SRC_DOMAIN" in template:
+            src_dst = "SRC"
+        elif "DST_DOMAIN" in template:
+            src_dst = "DST"
+        else:
+            src_dst = ""
+            continue #################### DOMAIN IS MANDATORY IN YANG FILE ############################
+        domain = template[src_dst + "_DOMAIN"][:7]
+        hash = hashlib.sha256((json.dumps(template["RULE_TEMPLATE"], sort_keys=True)).encode(encoding="utf-8", errors="replace")).hexdigest()[:5]
         table = template["ACL_TABLE_NAME"]
+            
+        rule_name = PREFIX + src_dst + "_" + domain + "_" + hash + "_"
 
         matching_rules = {}
         nums_used = set()
 
-        if table in existing_rules and domain in existing_rules[table] and hash in existing_rules[table][domain]:
-            for rule in existing_rules[table][domain][hash]:
+        if table in existing_rules and src_dst in existing_rules[table] and domain in existing_rules[table][src_dst] and hash in existing_rules[table][src_dst][domain]:
+            for rule in existing_rules[table][src_dst][domain][hash]:
                 matching_rules[rule["ip"]] = rule
                 nums_used.add(rule["number"])
         
-        #new_ips = nslookup(template["DOMAIN"]) # Create mockup of nslookup
-        new_ips = set(fake_nslookup(template["DOMAIN"]))
+        #new_ips = nslookup(template[src_dst + "_DOMAIN"]) # Create mockup of nslookup
+        new_ips = set(fake_nslookup(template[src_dst + "_DOMAIN"]))
         extra_old_ips = set(matching_rules.keys()).difference(new_ips)
-        extra_new_ips = new_ips.difference(matching_rules.keys())
+        extra_new_ips = new_ips.difference(set(matching_rules.keys()))
 
         last_checked_num = 1
         while len(extra_new_ips) > 0:
@@ -157,32 +187,33 @@ def updateRules():
                 nums_used.add(last_checked_num)
                 last_checked_num += 1
             path = table + "|" + rule_name + "_" + str(number)
-            patches.append(createPatch(path, "add", template["RULE_TEMPLATE"], new_ip))
+            patches.append(createPatch(path, "add", template["RULE_TEMPLATE"], new_ip, src_dst))
         
         while len(extra_old_ips) > 0:
             old_ip = extra_old_ips.pop()
             number = matching_rules[old_ip]["number"]
-            path = table + "|" + rule_name + "_" + str(number)
+            path = table + "|" + rule_name + str(number)
             patches.append(createPatch(path, "remove"))
 
-        seen_templates.add(tuple([template["ACL_TABLE_NAME"], template["DOMAIN"], hash]))
+        seen_templates.add(tuple([template["ACL_TABLE_NAME"], src_dst, template[src_dst + "_DOMAIN"], hash]))
 
     # Checks all existing rules and confirms that there is a template for that rule
-    for table, domains in existing_rules.items():
-        for domain, hashes in domains.items():
-            for hash, rule_list in hashes.items():
-                if tuple([table, domain, hash]) not in seen_templates:
-                    for rule in rule_list:
-                        path = table + "|" + PREFIX + domain + "_" + hash + "_" + str(rule["number"])
-                        patches.append(createPatch(path, "remove"))
+    for table, src_dsts in existing_rules.items():
+        for src_dst, domains in src_dsts.items():
+            for domain, hashes in domains.items():
+                for hash, rule_list in hashes.items():
+                    if tuple([table, domain, hash]) not in seen_templates:
+                        for rule in rule_list:
+                            path = table + "|" + PREFIX + rule["src_dst"] + "_" + domain + "_" + hash + "_" + str(rule["number"])
+                            patches.append(createPatch(path, "remove"))
 
     return patches
 
-patches = updateRules() # geneate all the patches to update the ACL_RULES
+patches = updateRules() # generate all the patches to update the ACL_RULES
 
 with open("patches.json", "w") as f: # dump the patches in a json
     json.dump(patches, f, indent=4)
 
-subprocess.run("sudo config apply-patch patches.json", shell=True) # apply the patches using GCU
+# subprocess.run("sudo config apply-patch patches.json", shell=True) # apply the patches using GCU
 
-subprocess.run("sudo rm patches.json", shell=True) # delete the json
+# subprocess.run("sudo rm patches.json", shell=True) # delete the json
---
+++ fqdn_yang.py
@@ -0,0 +1,154 @@
+# Open and read original sonic-acl.yang file
+directory_path = "\\etc\\sonic\\yang-models\\"
+with open(directory_path + "sonic-acl.yang", "r") as f:
+    acl_yang = f.readlines()
+
+fqdn_yang = []
+
+# Define variables for deleting extraneous containers we don't need
+extra_container = False
+count = 0
+
+# Iterate through every line of sonic-acl.yang
+for i in range(len(acl_yang)):
+    # Get the new line
+    new_line = acl_yang[i]
+
+    # Add sonic-acl import before all other import statements
+    if "import ietf-inet-types" in new_line:
+        acl_import = ["\timport sonic-acl {\n",
+                      "\t\tprefix acl;\n",
+                      "\t}\n",
+                      "\n"]
+        fqdn_yang.extend(acl_import)
+
+    # Modify revision date of sonic-acl import and also add the revision date for the current file
+    if "revision " in new_line:
+        revision_date = new_line[len("\trevision "):-3]
+        for i in range(len(fqdn_yang)):
+            if fqdn_yang[i] == "\timport sonic-acl {\n":
+                fqdn_yang.insert(i+2, "\t\trevision-date " + revision_date + ";\n")
+                break    
+        new_line = new_line.replace(revision_date, "2024-06-28")            
+
+    # # Add typedefs for the fqdn-ipv4 and fqdn-ipv6 prefixes
+    # if "container sonic-acl {" in new_line:
+    #     type_def = ['\ttypedef fqdn-ipv4-prefix {\n',
+    #                 '\t\ttype string {\n',
+    #                 "\t\t\tpattern '((([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}'\n",
+    #                 "\t\t\t\t+ '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'\n",
+    #                 "\t\t\t\t+ '/(([0-9])|([1-2][0-9])|(3[0-2])))'\n",
+    #                 "\t\t\t\t+ '|\\(\\(FQDN_IP\\)\\)';\n",
+    #                 '\t\t}\n',
+    #                 '\t}\n',
+    #                 '\n',
+    #                 '\ttypedef fqdn-ipv6-prefix {\n',
+    #                 '\t\ttype string {\n',
+    #                 "\t\t\tpattern '(((:|[0-9a-fA-F]{0,4}):)([0-9a-fA-F]{0,4}:){0,5}'\n",
+    #                 "\t\t\t\t+ '((([0-9a-fA-F]{0,4}:)?(:|[0-9a-fA-F]{0,4}))|'\n",
+    #                 "\t\t\t\t+ '(((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.){3}'\n",
+    #                 "\t\t\t\t+ '(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])))'\n",
+    #                 "\t\t\t\t+ '(/(([0-9])|([0-9]{2})|(1[0-1][0-9])|(12[0-8]))))'\n",
+    #                 "\t\t\t\t+ '|\\(\\(FQDN_IP\\)\\)';\n",
+    #                 "\t\t\tpattern '((([^:]+:){6}(([^:]+:[^:]+)|(.*\\..*)))|'\n",
+    #                 "\t\t\t\t+ '((([^:]+:)*[^:]+)?::(([^:]+:)*[^:]+)?)'\n",
+    #                 "\t\t\t\t+ '(/.+))'\n",
+    #                 "\t\t\t\t+ '|\\(\\(FQDN_IP\\)\\)';\n",
+    #                 '\t\t}\n',
+    #                 '\t}\n',
+    #                 '\n']
+    #     fqdn_yang.extend(type_def)
+
+    # Modify RULE_NAME leaf to TEMPLATE_NAME leaf and add domain leaf
+    if "leaf RULE_NAME" in new_line:
+        new_leafs = ['\t\t\t\tleaf TEMPLATE_NAME {\n',
+                              '\t\t\t\t\ttype string {\n',
+                              '\t\t\t\t\t\tpattern "^(?!.*_).{1,255}$";\n'
+                              '\t\t\t\t\t}\n',
+                              '\t\t\t\t}\n',
+                              '\n',
+                              '\t\t\t\tchoice src_dst_domain {\n',
+                              '\t\t\t\t\tmandatory true;\n',
+                              '\n',
+                              '\t\t\t\t\tleaf SRC_DOMAIN {\n',
+                              '\t\t\t\t\t\ttype inet:domain-name;\n'
+                              '\t\t\t\t\t}\n',
+                              '\n',
+                              '\t\t\t\t\tleaf DST_DOMAIN {\n',
+                              '\t\t\t\t\t\ttype inet:domain-name;\n'
+                              '\t\t\t\t\t}\n',
+                              '\t\t\t\t}\n',
+                              '\n']
+        fqdn_yang.extend(new_leafs)
+        extra_container = True
+
+    # # Add DOMAIN leaf
+    # if "leaf PACKET_ACTION" in new_line:
+    #     leaf_domain = [#'\t\t\t\tleaf DOMAIN {\n',
+    #                 #    '\t\t\t\t\ttype inet:domain-name;\n'
+    #                 #    '\t\t\t\t}\n',
+    #                    '\t\t\t\tchoice src_dst_domain {\n',
+    #                    '\t\t\t\t\tmandatory true;\n',
+    #                    '\n',
+    #                    '\t\t\t\t\tleaf SRC_DOMAIN {\n',
+    #                    '\t\t\t\t\t\ttype inet:domain-name;\n'
+    #                    '\t\t\t\t\t}\n',
+    #                    '\n',
+    #                    '\t\t\t\t\tleaf DST_DOMAIN {\n',
+    #                    '\t\t\t\t\t\ttype inet:domain-name;\n'
+    #                    '\t\t\t\t\t}\n',
+    #                    '\t\t\t\t}\n',
+    #                    '\n']
+    #     fqdn_yang.extend(leaf_domain)
+
+    # Change relative paths to absolute paths
+    if "../.." in new_line:
+        new_line = new_line.replace("../..", "/sonic-acl")
+        new_line = new_line.replace("/", "/acl:")
+        
+    # Replace all necessary text in every line after addding in all new text
+    new_line = new_line.replace("sonic-acl", "sonic-fqdn-acl-template")
+    
+    new_line = new_line.replace(":sonic-fqdn-acl-template", ":sonic-acl")
+
+    new_line = new_line.replace("prefix acl", "prefix fqdn")
+
+    new_line = new_line.replace("ACL_RULE", "FQDN_ACL_RULE_TEMPLATE")
+        
+    new_line = new_line.replace("ACL_TABLE_NAME RULE_NAME", "TEMPLATE_NAME")
+
+    # new_line = new_line.replace("inet:ipv4-prefix", "fqdn-ipv4-prefix")
+
+    # new_line = new_line.replace("inet:ipv6-prefix", "fqdn-ipv6-prefix")
+
+    # Change any mention remaining of "ACL" to "FQDN"
+    new_line = new_line.replace("ACL ", "FQDN ")
+
+    # Flag an unnecessary container
+    if "container" in new_line and ("sonic-fqdn-acl-template" not in new_line and "FQDN_ACL_RULE_TEMPLATE" not in new_line):
+        extra_container = True
+
+    # Skip lines within an extraneous container
+    if extra_container:
+        comment = False
+        for c in new_line:
+            if not comment and c == "*":
+                comment = True
+            elif c == "*":
+                comment = False
+            elif not comment and c == "{":
+                count += 1
+            elif not comment and c == "}":
+                count -= 1
+        
+        # If all open curly braces are closed, then the extraneous container ended, skip one last line
+        if count == 0:
+            extra_container = False
+        continue        
+
+    # Add the newly changed line to the new yang file text
+    fqdn_yang.append(new_line)
+
+# Write to the new sonic-fqdn-acl-template.yang file
+with open(directory_path + "sonic-fqdn-acl-template.yang", "w") as f:
+    f.writelines(fqdn_yang)
